2406651c2163a375eab55a808a4647064fcc593f
elisey2006 <eliseysidenko@gmail.com>
2020-10-14 17:22:59 +1000

    2 занятие

--- main.py
+++ main.py
@@ -7,13 +7,50 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
-pygame.init()
-pygame.display.set_capiton("угадай число")
- screen = pygame.display.set_mode(W, H)
- pygame.mouse.set_visible(False)
+numeral = 
+move = 1
+block = 0
+start = 1
+OUTSIDE_BG = (0, -100)
 
-    run = True
-    while run:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                run = False
+pygame.init()
+pygame.display.set_caption("угадай число")
+screen = pygame.display.set_mode((W, H))
+pygame.mouse.set_visible(False)
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+
+
+font = pygame.ront.SysFont("Arial", 28, True, False)      
+font2 = pygame.ront.SysFont("Arial", 14, True, False) 
+font_box = pygame.ront.Surface()
+font = pygame.ront.SysFont("Arial", 28, True, False)      
+
+bg = pygame.image.load("image/room.png")
+bg_rect = bg.get_rect(topleft=(0, 0))
+
+screen.blit(bg, bg_rect)
+screen.blit(cat, cat_rect)
+screen.blit(dog, dog_rect)
+screen.blit(owl, owl_rect)
+screen.blit(font_box, font_rect)
+font_box.fill(SILVER)
+pygame.display.update()
+
+
+cat = pygame.image.load("image/cat.png")
+cat_rect = cat.get_rect(center=(70, 220))
+
+dog = pygame.image.load("image/dog.png")
+dog_rect = dog.get_rect(center=(210, 120))
+
+owl = pygame.image.load("image/owl.png")
+owl_rect = owl.get_rect(center=(210, 120))
+
+dialog = pygame.image.load("image/dialog.png")
+dialog_rect = dialog.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_cat_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
