fadab09c74ca5023e15fee49b6ed898392c6a9a6
elisey2006 <eliseysidenko@gmail.com>
2020-10-19 17:38:00 +1000

    2 занятие

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
-numeral = 
+numeral = ''
 move = 1
 block = 0
 start = 1
@@ -22,12 +22,16 @@ while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
+        elif e.type == pygame.K_ESCAPE:
+            run = False
+            elif e.unicode.isdecimal() and block == 0
+            numeral 
 
 
 font = pygame.ront.SysFont("Arial", 28, True, False)      
-font2 = pygame.ront.SysFont("Arial", 14, True, False) 
-font_box = pygame.ront.Surface()
-font = pygame.ront.SysFont("Arial", 28, True, False)      
+font2 = pygame.ront.SysFont("Arial", 14, False, True) 
+font_box = pygame.ront.Surface(w-30, font.get_height())
+font_rect = font_box.get_rect(center=(w // 2, H - 30))
 
 bg = pygame.image.load("image/room.png")
 bg_rect = bg.get_rect(topleft=(0, 0))
@@ -53,4 +57,5 @@ owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load("image/dialog.png")
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
-dialog_cat_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x - dialog_rect.w // 2, owl_rect.y - dialog_rect.h)
