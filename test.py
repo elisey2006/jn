5f9896199ca490a9a9045041dd65413ae88e0c14
elisey2006 <eliseysidenko@yandex.ru>
2020-10-28 17:36:09 +1000

    .

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
-import pygame
-W, H = 800, 600
-BLUE = (192, 192, 192)
-BLACK = (50, 50, 50)
-numeral = ''
-start = 1
+import pygame as pg
 
-pygame.init()
-pygame.display.set_caption('тест')
-screen = pygame.display.set_mode((W, H))
+SIZE = WIDTH, HEIGHT = 640, 480
+speed_x, speed_y = 5, 5
+BLACK = (0, 0, 0)
 
-run = True
-while run:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            run = False
-        elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_ESCAPE:
-                run = False
-            
-screen.fill
+pg.init()
+pg.display.set_caption("ball")
+screen = pg.display.set_mode()
+FPS = 60
+clock = pg.time.Clock()
+
+
+ball = pg.image.load("img/ball.png")
+ball_rect = ball.get_rect()
+#print(ball_rect)
+
+while True:
+    for e in pg.event.get():
+        if e.type == pg.QUIT or \
+            e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+            sys.exit(0)
+
+        ball_rect = ball_rect.move(speed_x, speed_y)
+        if ball_rect.left < 0 or ball_rect.right < WIDTH:
+            speed_x = -speed_x
+        if ball_rect.top < 0 or ball_rect.bottom > HEIGHT:
+            speed_y = -speed_y
+
+screen.fill(BLACK)
+screen.blit(ball, ball_rect)
+pg.display.update()
+clock.tick(FPS)
+pg.display.set_caption(f"ball  FPS:{int(clock.get_fps())}")
